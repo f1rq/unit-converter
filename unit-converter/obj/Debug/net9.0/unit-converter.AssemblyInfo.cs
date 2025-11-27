f44d546dca02cdb3c9a16b4c28dba7bd1fcf0018
f1rq <f.czudaj@tutanota.com>
2025-11-27 10:01:21 +0100

    Added .gitignore.

--- unit-converter/obj/Debug/net9.0/unit-converter.AssemblyInfo.cs
+++ unit-converter/obj/Debug/net9.0/unit-converter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("unit-converter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb1fc9ee6b7f455d72376abc5c4003c1c3f4667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb3c79d9b863c3fafacdea9388502f85ae19d856")]
 [assembly: System.Reflection.AssemblyProductAttribute("unit-converter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("unit-converter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
