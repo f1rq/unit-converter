b37fc6c48a21ea77d5acae97dbb696f4cd02f637
f1rq <f.czudaj@tutanota.com>
2025-12-01 12:35:21 +0100

    Fecth currencies from api on app launch.

--- unit-converter/App.axaml.cs
+++ unit-converter/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using unit_converter.Units;
 
 namespace unit_converter;
 
@@ -9,6 +10,7 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+        _ = CurrencyRates.UpdateRatesAsync();
     }
 
     public override void OnFrameworkInitializationCompleted()
