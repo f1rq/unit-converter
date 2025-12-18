6e47a93ba03710efe50f60847dc6195bdc09eed1
f1rq <f.czudaj@tutanota.com>
2025-12-18 15:38:22 +0100

    fix: Fix data conversion factors
    e.g. 1024MB showed 1.02GB

--- unit-converter/Units/DataUnits.cs
+++ unit-converter/Units/DataUnits.cs
@@ -23,19 +23,19 @@ public class DataUnits
         new Dictionary<string, DataUnit>
         {
             ["B"] = new("B", "B - byte", 1),
-            ["KB"] = new("KB", "KB - kilobyte", 1e3),
-            ["MB"] = new("MB", "MB - megabyte", 1e6),
-            ["GB"] = new("GB", "GB - gigabyte", 1e9),
-            ["TB"] = new("TB", "TB - terabyte", 1e12),
-            ["PB"] = new("PB", "PB - petabyte", 1e15),
-            
+            ["KB"] = new("KB", "KB - kilobyte", 1024),
+            ["MB"] = new("MB", "MB - megabyte", 1024 * 1024),
+            ["GB"] = new("GB", "GB - gigabyte", 1024 * 1024 * 1024),
+            ["TB"] = new("TB", "TB - terabyte", 1024L * 1024 * 1024 * 1024),
+            ["PB"] = new("PB", "PB - petabyte", 1024L * 1024 * 1024 * 1024 * 1024),
+
             ["b"] = new("b", "b - bit", 1.0 / 8),
-            ["Kb"] = new("Kb", "Kb - kilobit", 1e3 / 8),
-            ["Mb"] = new("Mb", "Mb - megabit", 1e6 / 8),
-            ["Gb"] = new("Gb", "Gb - gigabit", 1e9 / 8),
-            ["Tb"] = new("Tb", "Tb - terabit", 1e12 / 8),
-            ["Pb"] = new("Pb", "Pb - petabit", 1e15 / 8),
-            
+            ["Kb"] = new("Kb", "Kb - kilobit", 1024.0 / 8),
+            ["Mb"] = new("Mb", "Mb - megabit", (1024.0 * 1024) / 8),
+            ["Gb"] = new("Gb", "Gb - gigabit", (1024.0 * 1024 * 1024) / 8),
+            ["Tb"] = new("Tb", "Tb - terabit", (1024.0 * 1024 * 1024 * 1024) / 8),
+            ["Pb"] = new("Pb", "Pb - petabit", (1024.0 * 1024 * 1024 * 1024 * 1024) / 8),
+
             ["KiB"] = new("KiB", "KiB - kibibyte", 1024),
             ["MiB"] = new("MiB", "MiB - mebibyte", 1024 * 1024),
             ["GiB"] = new("GiB", "GiB - gibibyte", 1024 * 1024 * 1024),
